7640c390e9696d07a05a8270ab79163faf62af76
Tom Chomiak <tom.chomiak@gmail.com>
2022-01-20 09:25:31 -0600

    Add turn server info

--- public/js/main.js
+++ public/js/main.js
@@ -21,16 +21,16 @@ if(location.href.substr(0,5) !== 'https')
 /**
  * RTCPeerConnection configuration 
  */
-const configuration = {
+ const configuration = {
     "iceServers": [{
             "urls": "stun:stun.l.google.com:19302"
         },
         // public turn server from https://gist.github.com/sagivo/3a4b2f2c7ac6e1b5267c2f1f59ac6c6b
         // set your own servers here
         {
-            url: 'turn:192.158.29.39:3478?transport=udp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
+            urls: 'turn:turn.panoskin.com:3478',
+            credential: 'u1',
+            username: 'p1'
         }
     ]
 }
